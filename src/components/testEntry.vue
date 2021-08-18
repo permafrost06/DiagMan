62210d7c5f337bd789998f11f1da400d3938f5e3
permafrost06 <zahin06@gmail.com>
2021-08-18 14:37:40 +0600

    created settings page with tests edit

---
+++ src/components/testEntry.vue
@@ -0,0 +1,59 @@
+<template>
+  <li>
+    <span v-if="edit">
+      <input type="text" v-model="newName" />
+      <input type="text" v-model="newCost" />
+      <button @click="updateEntry">Save</button>
+    </span>
+    <span v-else>
+      {{ name }}: {{ cost }}
+
+      <button @click="editEntry">Edit</button
+      ><button @click="deleteEntry">Delete</button>
+    </span>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    _id: String,
+    name: String,
+    cost: String,
+  },
+  data() {
+    return {
+      edit: false,
+      newName: this.name,
+      newCost: this.cost,
+    };
+  },
+  methods: {
+    editEntry() {
+      this.edit = true;
+    },
+    updateEntry() {
+      this.$emit("test-update", {
+        _id: this._id,
+        name: this.newName,
+        cost: this.newCost,
+      });
+      this.edit = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  width: auto;
+  height: auto;
+  padding: 0.4rem 0.75rem;
+  margin: 0.5rem;
+}
+
+input {
+  width: 10rem;
+  margin: 0.5rem;
+}
+</style>
---
+++ src/views/Settings.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="settings-page">
+    <h1>Settings</h1>
+    <div class="settings-tests">
+      <h2>Tests</h2>
+      <ul v-for="test in tests" :key="test._id">
+        <testEntry v-bind="test" />
+      </ul>
+      <span v-if="add"
+        ><input placeholder="Test Name" v-model="newTestName" /><input
+          placeholder="Test Cost"
+          v-model="newTestCost"
+        />
+        <button @click="addTestToDB">Add</button></span
+      >
+      <button v-else @click="addTest">Add New Test</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import testEntry from "../components/testEntry.vue";
+export default {
+  components: { testEntry },
+  data() {
+    return {
+      add: false,
+      tests: [
+        {
+          _id: 1,
+          name: "test_1",
+          cost: 100,
+        },
+        {
+          _id: 2,
+          name: "test_2",
+          cost: 200,
+        },
+      ],
+    };
+  },
+  methods: {
+    addTest() {
+      this.add = true;
+    },
+    addTestToDB() {
+      console.log(this.newTestName, this.newTestCost);
+      this.newTestName = "";
+      this.newTestCost = "";
+      this.add = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.settings-page {
+  margin: 1rem 3rem;
+
+  button {
+    height: auto;
+    width: auto;
+    padding: 0.4rem 1rem;
+  }
+}
+
+.settings-tests {
+  ul {
+    list-style: none;
+  }
+
+  input {
+    width: 10rem;
+    margin: 0.5rem;
+  }
+}
+</style>
