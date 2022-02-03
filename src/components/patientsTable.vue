190c273826085567bfc570279a87843c98669e3e
permafrost06 <zahin06@gmail.com>
2022-02-03 12:53:13 +0600

    version bump for auto update test

--- src/components/patientsTable.vue
+++ src/components/patientsTable.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1 style="display:inline-block;margin: 1rem">Pending Patients</h1>
+  <h1 style="display:inline-block;margin: 1rem">
+    Pending Patients | Updated version
+  </h1>
   <router-link :to="{ name: 'AddRecord' }">
     <button style="width: auto; margin: 1rem; padding: .25rem 1rem;">
       Add Patient
