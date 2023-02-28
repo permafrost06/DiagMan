49a0aa38531af30ca2f0106fd24451d6f66b6e46
Saad <sakib.saad.khan@gmail.com>
2023-02-28 15:55:46 +0600

    [*] Cleaned default files

---
+++ src/layout/Front.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup>
+import { RouterView } from 'vue-router'
+</script>
+<template>
+    <RouterView/>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,4 @@
+<template>
+    This is home <br/>
+    <RouterLink :to="{name: 'components'}" >Components</RouterLink>
+</template>
