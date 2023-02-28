17a21ed21de51ea1758996fd2a641f6fc99b83cd
Saad <sakib.saad.khan@gmail.com>
2023-02-28 16:19:19 +0600

    [+] Added initial table example

--- src/components/TableX.vue
+++ src/components/TableX.vue
@@ -1,16 +1,48 @@
 <script lang="ts">
 /*Declearations had to be separated because of volar error*/
 
-type TableCol = string;
+type TableCol = {
+    label: string,
+    name: string,
+    className?: string,
+    thClass?: string,
+    width?: string
+};
 interface TableXProps{
-    cols: TableCol
+    cols: TableCol[],
+    data: any[]
 }
 </script>
 
 <script setup lang="ts">
-defineProps<TableXProps>();
+const props = defineProps<TableXProps>();
+/*
+props.cols.forEach((col, idx)=>{
+    if(typeof col === 'string'){
+        props.cols[idx] = {
+            label: col
+        }
+    }
+})
+*/
 </script>
 
 <template>
-    <div>This is a table with cols: {{ cols }}</div>
+    <div>
+        <table>
+            <colgroup>
+                <col v-for="cprops in cols" :width="cprops.width"/>
+            </colgroup>
+            <thead>
+                <tr>
+                    <th v-for="cprops in cols" :class="cprops.thClass">{{ cprops.label }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="row in data">
+                    <td v-for="cprops in cols" :class="cprops.className">{{ row[cprops.name] }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
--- src/views/Components.vue
+++ src/views/Components.vue
@@ -20,7 +20,28 @@ async function checkCmp(){
 
 const components:{[key: string]: any} = {
     TableX: {
-        cols: "Not finalized"
+        cols: [
+            {
+                name: 'no',
+                label: 'No'
+            },
+            {
+                name: 'name',
+                label: 'Name'
+            },
+            {
+                name: 'email',
+                label: 'Email',
+                width: '50%'
+            }
+        ],
+        data: [
+            {no: 1, name: 'Some user', email: 'someuser@gmail.com'},
+            {no: 2, name: 'Some user', email: 'someuser@gmail.com'},
+            {no: 3, name: 'Some user', email: 'someuser@gmail.com'},
+            {no: 4, name: 'Some user', email: 'someuser@gmail.com'},
+            {no: 5, name: 'Some user', email: 'someuser@gmail.com'},
+        ]
     }
 }
 
