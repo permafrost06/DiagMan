d1ba67a8b8d31504d41808fa79b8cadd2d23dccd
Saad <sakib.saad.khan@gmail.com>
2023-03-09 18:42:53 +0600

    added checkbox

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -20,8 +20,11 @@ export interface TableProps extends TableHTMLAttributes {
 }
 
 const tableRef = ref<HTMLTableElement>();
+const checkBoxRef = ref<HTMLInputElement>();
+const checkBoxState = ref<boolean | "indeterminate">(false);
 
-defineProps<TableProps>();
+const props = defineProps<TableProps>();
+const data = ref<any[]>(props.data);
 
 onMounted(() => {
     window.addEventListener("mouseup", dragEnd);
@@ -87,6 +90,44 @@ const dragEnd = () => {
         window.removeEventListener("mousemove", changeWidth);
     }
 };
+
+const onRowCheckBoxChange = (evt: Event, idx: number) => {
+    //@ts-ignore
+    const value = evt.target.checked;
+    let allChecked = true;
+    let allNotCheked = true;
+
+    data.value = data.value.map((item, index) => {
+        if (index == idx) {
+            item.checked = value;
+        }
+        if (!item.checked) {
+            allChecked = false;
+        } else {
+            allNotCheked = false;
+        }
+
+        return item;
+    });
+
+    if (allChecked) {
+        checkBoxState.value = true;
+    } else if (allNotCheked) {
+        checkBoxState.value = false;
+    } else {
+        checkBoxState.value = "indeterminate";
+    }
+};
+
+const bulkCheckChange = (evt: Event) => {
+    //@ts-ignore
+    const value = evt.target.checked;
+    checkBoxState.value = value;
+    data.value = data.value.map((item) => {
+        item.checked = value;
+        return item;
+    });
+};
 </script>
 
 <template>
@@ -94,6 +135,15 @@ const dragEnd = () => {
         <table ref="tableRef" v-bind="$attrs" class="tablex">
             <thead>
                 <tr>
+                    <th>
+                        <input
+                            type="checkbox"
+                            :checked="checkBoxState === true"
+                            :indeterminate="checkBoxState === 'indeterminate'"
+                            @click="bulkCheckChange"
+                            ref="checkBoxRef"
+                        />
+                    </th>
                     <th
                         v-for="(cprops, idx) in cols"
                         :style="`width: ${cprops.width}`"
@@ -107,6 +157,13 @@ const dragEnd = () => {
             </thead>
             <tbody>
                 <tr v-for="(row, row_idx) in data" :key="row_idx">
+                    <td>
+                        <input
+                            type="checkbox"
+                            :checked="row.checked"
+                            @change="(evt) => onRowCheckBoxChange(evt, row_idx)"
+                        />
+                    </td>
                     <td
                         v-for="(cprops, col_idx) in cols"
                         :class="cprops.className"
