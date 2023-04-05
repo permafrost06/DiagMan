a11273d522c2b5aca52b0e5027ef916651624765
Zahin Azmayeen <zahin06@gmail.com>
2023-04-05 16:17:48 +0600

    Better number array generation

--- src/views/components/PaginationView.vue
+++ src/views/components/PaginationView.vue
@@ -1,11 +1,8 @@
 <script lang="ts" setup>
 import Pagination from "@/components/Pagination.vue";
-const items: number[] = [];
-
-for (let i = 0; i < 10; i++) {
-    items.push(i);
-}
+const items: number[] = Array.from({ length: 10 }, (_, i) => i);
 </script>
+
 <template>
     <Pagination :items="[]" :current="10" :all-items="items" />
 </template>
