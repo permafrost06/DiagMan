71947e86e16fac3ee78551983cce23e267106fb8
Saad <sakib.saad.khan@gmail.com>
2023-04-10 12:45:46 +0600

    added props demonstration

--- src/views/components/PaginationView.vue
+++ src/views/components/PaginationView.vue
@@ -1,9 +1,86 @@
 <script lang="ts" setup>
 import Pagination from "@/components/Pagination.vue";
 import { ref } from "vue";
-const page = ref<number>(1);
+const page1 = ref<number>(1);
+const page2 = ref<number>(1);
+
+const onEachSide = ref<number>(2);
+const itemCount = ref<number>(100);
+
+const pages = ref<number>(10);
+const pageSize = ref<number>(10);
 </script>
 
 <template>
-    <Pagination :pages="10" :item-count="100" v-model="page" />
+    <div class="opt-group">
+        <div class="option">
+            <p>onEachSide: {{ onEachSide }}</p>
+            <button @click="onEachSide++">+</button>
+            <button @click="onEachSide > 0 && onEachSide--">-</button>
+        </div>
+
+        <div class="option">
+            <p>Item Count:</p>
+            <input type="number" v-model="itemCount" />
+        </div>
+    </div>
+
+    <div class="opt-group title">
+        <div class="option">
+            <p>pages: {{ pages }}</p>
+            <button @click="pages++">+</button>
+            <button @click="pages > 1 && pages--">-</button>
+        </div>
+    </div>
+    <div class="pagination-wrapper">
+        <Pagination
+            v-model="page1"
+            :pages="pages"
+            :item-count="itemCount"
+            :on-each-side="onEachSide"
+        />
+    </div>
+
+    <div class="opt-group title">
+        <div class="option">
+            <p>pageSize: {{ pageSize }}</p>
+            <button @click="pageSize++">+</button>
+            <button @click="pageSize > 1 && pageSize--">-</button>
+        </div>
+    </div>
+    <div class="pagination-wrapper">
+        <Pagination
+            v-model="page2"
+            :page-size="pageSize"
+            :item-count="itemCount"
+            :on-each-side="onEachSide"
+        />
+    </div>
 </template>
+<style scoped>
+.opt-group {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+.title {
+    margin-top: 100px;
+}
+
+.option {
+    margin: 0 2px;
+    display: flex;
+    border: 1px solid black;
+    padding: 8px;
+}
+
+.option p {
+    margin: 0 5px;
+}
+
+.pagination-wrapper {
+    width: max-content;
+    margin: 20px auto;
+}
+</style>
