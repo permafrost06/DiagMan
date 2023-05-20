d3228b374cc873ad67ca1a777c00bba1d008b0c1
Saad <sakib.saad.khan@gmail.com>
2023-05-20 15:14:30 +0600

    added Chart component

---
+++ src/components/chart/Chart.vue
@@ -0,0 +1,16 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script lang="ts" setup>
+import LineChart, { type DataPoint } from "./LineChart.vue";
+interface ChartProps {
+    type: "line";
+    data: DataPoint[][];
+}
+defineProps<ChartProps>();
+
+const chartMap = {
+    line: LineChart,
+};
+</script>
+<template>
+    <component :data-groups="data" :is="chartMap[type]" />
+</template>
