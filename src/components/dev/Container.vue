97492e20b546fb3132da864c54c9d930cd0dc1e3
Saad <sakib.saad.khan@gmail.com>
2023-07-18 16:39:48 +0600

    Added dev components for testing

---
+++ src/components/dev/Container.vue
@@ -0,0 +1,13 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts"></script>
+<template>
+    <div>
+        <slot></slot>
+    </div>
+</template>
+<style scoped>
+div {
+    max-width: 700px;
+    margin: 10px auto;
+}
+</style>
---
+++ src/components/dev/TextArea.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+    modelValue: string;
+}>();
+const emit = defineEmits<{
+    (e: "update:modelValue", page: number): void;
+}>();
+const updated = (evt: any) => {
+    emit("update:modelValue", evt.target.value);
+};
+</script>
+<template>
+    <textarea :value="modelValue" @input="updated"></textarea>
+</template>
+<style scoped>
+textarea {
+    display: block;
+    min-height: 80px;
+    width: 100%;
+    border-radius: 5px;
+    border: 1px solid #a7a7a7;
+    padding: 5px;
+}
+</style>
