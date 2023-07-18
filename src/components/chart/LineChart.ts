a69e29bcfe5c4e7e56a695deed14930d795c3c37
Saad <sakib.saad.khan@gmail.com>
2023-07-18 15:30:59 +0600

    LineChart with proper legend & sizing

--- src/components/chart/LineChart.ts
+++ src/components/chart/LineChart.ts
@@ -89,6 +89,12 @@ export class LineChart {
     protected legendColors: string[] = [];
     protected legends: string[] = [];
 
+    //TODO: Figure a way out to make this things dynamic
+    protected xAxisHeight = 20;
+    protected yAxisWidth = 30;
+    protected topSpace = 10;
+    protected rightSpace = 10;
+
     protected legendOpts: LegendProps = {
         fontSize: 14,
         gap: {
@@ -139,54 +145,153 @@ export class LineChart {
         const legendSize = this.drawLegends(remHeight, remWidth);
         remHeight -= legendSize.height;
         const axesLabelSize = this.drawAxesLabel(remHeight, remWidth);
+
         remHeight -= axesLabelSize.height;
         remWidth -= axesLabelSize.width;
 
-        let maxYVal = 0;
-        let maxXVal = 0;
+        const range = {
+            x: [0, 0],
+            y: [0, 0],
+        };
 
+        /**
+         * This could be achieved using `d3.extent` but that would
+         * take a round trip for each x and y.
+         */
         dataGroups.forEach((dataset) => {
             dataset.forEach((data) => {
-                if (data.y > maxYVal) {
-                    maxYVal = data.y;
-                }
+                range.x[0] = Math.min(range.x[0], data.x);
+                range.x[1] = Math.max(range.x[1], data.x);
+
+                range.y[0] = Math.min(range.y[0], data.y);
+                range.y[1] = Math.max(range.y[1], data.y);
             });
-            if (dataset[0] && dataset[0].y > 0) {
-                dataset.unshift({
-                    x: 0,
-                    y: 0,
-                });
-            }
-
-            if (dataset.length > 0) {
-                const last = dataset[dataset.length - 1];
-                if (last.y > 0) {
-                    dataset.push({
-                        x: last.x,
-                        y: 0,
-                    });
-                }
-            }
-
-            if (maxXVal < dataset.length - 1) {
-                maxXVal = dataset.length - 1;
-            }
         });
+
+        remWidth -= this.yAxisWidth;
+        remHeight -= this.xAxisHeight;
 
         this.xScale = d3
             .scaleLinear()
-            .domain([0, maxXVal])
-            .range([0, this.width]);
+            .domain(range.x)
+            .range([0, remWidth - this.rightSpace]);
         this.yScale = d3
             .scaleLinear()
-            .domain([0, maxYVal])
-            .range([this.height, 0]);
+            .domain(range.y)
+            .range([remHeight - this.topSpace, 0]);
+
+        this.drawAxes(remHeight, remWidth);
 
         const line = d3
             .line()
             .x((d: any) => this.xScale(d.x))
             .y((d: any) => this.yScale(d.y))
             .curve(d3.curveLinear);
+
+        const dataLayer = this.d3El
+            .append("g")
+            .attr(
+                "transform",
+                `translate(${this.width - remWidth}, ${this.topSpace})`
+            )
+            .attr("class", "data-layer");
+
+        const lineLayer = dataLayer.append("g").attr("class", "lines-layer");
+        const pointsLayer = dataLayer.append("g").attr("class", "points-layer");
+
+        const instance = this;
+
+        this.legendColors = [];
+
+        dataGroups.forEach((data, i) => {
+            this.legendColors.push(COLORS[i]);
+            const grad = this.initGradient(COLORS[i] + "55");
+            lineLayer
+                .selectAll(".line")
+                .data([data])
+                .join("path")
+                .attr("d", (d: any) => line(d))
+                .attr("fill", `url(${grad})`)
+                .attr("stroke", COLORS[i]);
+
+            pointsLayer
+                .append("g")
+                .selectAll(".data-point")
+                .data(data)
+                .enter()
+                .append("circle")
+                .attr("class", "data-point")
+                .attr("cx", (d: any) => this.xScale(d.x))
+                .attr("cy", (d: any) => this.yScale(d.y))
+                .attr("r", 2.5)
+                .attr("fill", COLORS[i])
+                .style("cursor", "pointer")
+                .on("mouseover", function (this: any, _evt, d) {
+                    instance.handleMouseOver(this, d, remWidth);
+                })
+                .on("mouseout", function (this: any) {
+                    instance.handleMouseOut(this);
+                });
+        });
+    }
+
+    public setLevels(levels: LevelArgs): LineChart {
+        if (typeof levels.x === "number") {
+            levels.x = {
+                count: levels.x,
+                unit: "",
+            };
+        }
+
+        if (typeof levels.y === "number") {
+            levels.y = {
+                count: levels.y,
+                unit: "",
+            };
+        }
+        //@ts-expect-error tada
+        this.levels = { ...this.levels, ...levels };
+        return this;
+    }
+
+    protected drawAxes(remHeight: number, remWidth: number) {
+        const xAxis = d3
+            .axisBottom(this.xScale)
+            .ticks(this.levels.x.count)
+            .tickFormat((i: any) => i + 1);
+        const xLayer = this.d3El
+            .append("g")
+            .attr("class", "axis x-axis")
+            .call(xAxis)
+            .attr(
+                "transform",
+                `translate(${this.width - remWidth}, ${remHeight})`
+            );
+
+        xLayer.select(".tick line").remove();
+        xLayer
+            .selectAll(".tick line")
+            .attr("class", "x-level")
+            .attr("y1", 0)
+            .attr("y2", -remHeight);
+
+        const yAxis = d3.axisLeft(this.yScale).ticks(this.levels.y.count);
+
+        const yLayer = this.d3El
+            .append("g")
+            .attr("class", "axis y-axis")
+            .call(yAxis)
+            .attr(
+                "transform",
+                `translate(${this.width - remWidth}, ${this.topSpace})`
+            );
+
+        yLayer.select(".tick line").remove();
+        yLayer
+            .selectAll(".tick line")
+            .attr("class", "y-level")
+            .attr("x1", 0)
+            .attr("x2", remWidth);
     }
 
     public setLegends(legends: string[]): LineChart {
@@ -317,7 +422,7 @@ export class LineChart {
             const bbox = label.node()?.getBBox();
             if (bbox) {
                 label.attr("y", remHeight - bbox.height);
-                size.height = bbox.height;
+                size.height = bbox.height + 10;
             }
         }
 
@@ -360,6 +465,52 @@ export class LineChart {
             .style("stop-color", colorCode) //start in blue
             .style("stop-opacity", 1);
         return id;
+    }
+
+    public handleMouseOver(element: any, d: any, remWidth: number) {
+        d3.select(element).attr("r", 3);
+
+        const label = `X: ${d.x.toFixed(2)}, Value: ${d.y.toFixed(2)}`;
+        const labelGroup = this.d3El
+            .append("g")
+            .attr("class", "data-label-group");
+        const hText = 20;
+        const wText = 5 * (label.length + 2);
+
+        let xPos = this.xScale(d.x) + this.width - remWidth;
+        let yPos = this.yScale(d.y) + this.topSpace;
+
+        if (xPos + wText > this.width) {
+            xPos = xPos - wText - 10;
+        }
+        if (yPos - hText - 23 < 0) {
+            yPos += hText;
+        }
+        labelGroup
+            .append("rect")
+            .attr("class", "data-label-bg")
+            .attr("x", xPos)
+            .attr("y", yPos - 23)
+            .attr("width", wText)
+            .attr("height", hText)
+            .attr("rx", 5) // Set the border radius
+            .attr("ry", 5);
+
+        labelGroup
+            .append("text")
+            .attr("class", "data-label")
+            .attr("x", xPos + 11)
+            .attr("y", yPos - 10)
+            .text(label)
+            .style("font-size", "10px")
+            .style("fill", "currentColor");
+    }
+
+    // Function to handle mouseout event
+    public handleMouseOut(element: any) {
+        d3.select(element).attr("r", 2.5);
+
+        this.d3El.selectAll(".data-label-group").remove();
     }
 }
 
