a00a57d2d67a3f5bda9cfe270a10b475d341263d
Saad <sakib.saad.khan@gmail.com>
2023-08-30 16:23:44 +0600

    added update functionality

--- src/views/tests/TestForm.vue
+++ src/views/tests/TestForm.vue
@@ -7,6 +7,7 @@ const isLoading = ref(false);
 const error = ref<string | null>(null);
 const message = ref<string | null>(null);
 const tests = ref<Array<Record<string, number | string>>>([]);
+const toEdit = ref<Record<string, number | string> | null>(null);
 
 onMounted(async () => {
     const res = await fetchApi(`${API_BASE}/tests`);
@@ -28,7 +29,13 @@ async function handleFormSubmit(evt: any) {
     if (res.success) {
         error.value = null;
         message.value = res.message!;
-        tests.value.push(res.data);
+        tests.value.push(res.rows[0]);
+        if (res.data?.updated) {
+            tests.value = tests.value.filter(
+                (test) => test.id != res.data.updated
+            );
+            toEdit.value = null;
+        }
     } else {
         error.value = res.message;
     }
@@ -41,19 +48,30 @@ async function handleFormSubmit(evt: any) {
             method="POST"
             @submit.prevent="handleFormSubmit"
         >
+            <input v-if="toEdit" type="hidden" name="id" :value="toEdit.id" />
             <p v-if="error">Error: {{ error }}</p>
             <p v-if="message">{{ message }}</p>
             <div>
                 <label for="price">Name</label>
-                <input type="text" id="name" name="name" />
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    :value="toEdit?.name"
+                />
             </div>
             <div>
                 <label for="price">Price</label>
-                <input type="number" id="price" name="price" />
+                <input
+                    type="number"
+                    id="price"
+                    name="price"
+                    :value="toEdit?.price"
+                />
             </div>
             <div>
                 <label for="size">Size</label>
-                <select name="size" id="size">
+                <select name="size" id="size" :value="toEdit?.size">
                     <option value="">Select Size</option>
                     <option value="small">Small</option>
                     <option value="medium">Medium</option>
@@ -62,7 +80,9 @@ async function handleFormSubmit(evt: any) {
                 </select>
             </div>
             <button :disabled="isLoading" type="submit">
-                {{ isLoading ? "Please wait..." : "Submit" }}
+                <span v-if="isLoading">Please wait...</span>
+                <span v-else-if="toEdit">Update</span>
+                <span v-else>Add</span>
             </button>
         </form>
         <div>
@@ -73,6 +93,7 @@ async function handleFormSubmit(evt: any) {
                     <th>Name</th>
                     <th>Price</th>
                     <th>Size</th>
+                    <th></th>
                 </tr>
                 <tr v-if="tests.length === 0">
                     <td colspan="4">No tests added yet!</td>
@@ -83,6 +104,10 @@ async function handleFormSubmit(evt: any) {
                         <td>{{ test.name }}</td>
                         <td>{{ test.price }}</td>
                         <td>{{ test.size }}</td>
+                        <td>
+                            <button @click="toEdit = test">Edit</button>
+                            <button>Delete</button>
+                        </td>
                     </tr>
                 </template>
             </table>
--- worker/src/routes/med-test.ts
+++ worker/src/routes/med-test.ts
@@ -4,28 +4,42 @@ import { RequestHandler } from '../router';
 import { validateFormData } from '../utils/helpers';
 
 const testSchema = z.object({
+	id: z.preprocess((val: any) => parseInt(val), z.number().min(0)).optional(),
 	name: z.string().nonempty(),
 	price: z.preprocess((val: any) => parseInt(val), z.number().min(0)),
 	size: z.enum(['small', 'medium', 'large', 'complex']),
+	status: z.enum(['active', 'updated', 'deleted']).default('active'),
 });
 
 export const addTest: RequestHandler = async ({ request, env, res }) => {
 	const data = await validateFormData(request, testSchema);
 
 	const db = getLibsqlClient(env);
-
+	if (data.id) {
+		await db.execute({
+			sql: "UPDATE `tests` SET status='updated' WHERE id=?",
+			args: [data.id],
+		});
+		res.setData({
+			updated: data.id,
+		});
+		delete data.id;
+	}
 	const qres = await db.execute({
-		sql: 'INSERT INTO `tests` (name, price, size) VALUES (:name, :price, :size)',
+		sql: 'INSERT INTO `tests` (name, price, size, status) VALUES (:name, :price, :size, :status)',
 		args: data,
 	});
-	res.setData({
-		id: qres.lastInsertRowid?.toString(),
-		...data,
-	});
+
+	res.setRows([
+		{
+			id: qres.lastInsertRowid?.toString(),
+			...data,
+		},
+	]);
 };
 
 export const listTests: RequestHandler = async ({ env, res }) => {
 	const db = getLibsqlClient(env);
-	const qres = await db.execute('SELECT * FROM `tests`');
+	const qres = await db.execute('SELECT * FROM `tests` WHERE status="active"');
 	res.setRows(qres.rows);
 };
