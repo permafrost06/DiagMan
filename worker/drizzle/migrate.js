230ec9dbbdd3d44a8f8bf2f64254a05bfda0ca19
Saad <sakib.saad.khan@gmail.com>
2023-08-31 23:20:21 +0600

    use local file to track migrations

--- worker/drizzle/migrate.js
+++ worker/drizzle/migrate.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-undef */
 const { config } = require('dotenv');
 const { createClient } = require('@libsql/client/web');
-const { readdirSync, readFileSync } = require('fs');
+const { readdirSync, readFileSync, existsSync, writeFileSync } = require('fs');
+
+const storePath = './migrations/meta/_completed.json';
 
 config({
 	path: '.dev.vars',
 });
+
 const url = process.env.TURSO_DB_URL?.trim();
 if (url === undefined) {
 	throw new Error('TURSO_DB_URL env var is not defined');
@@ -22,47 +25,29 @@ async function main() {
 	const allMigrations = readdirSync('./migrations')
 		.filter((path) => path.endsWith('.sql'))
 		.sort();
-	let migrations = [];
-	try {
-		const res = await db.execute('SELECT * FROM `migrations`');
-		const done = res.rows.map((row) => row.name);
-		migrations = allMigrations.filter((file) => done.indexOf(file) === -1);
-	} catch (error) {
-		if (error.code === 'SQLITE_UNKNOWN') {
-			await createMigrationsTable();
-			migrations = allMigrations;
-		} else {
-			console.log(error.message);
-			return;
-		}
-	}
+	const completedMigrations = existsSync(storePath) ? JSON.parse(readFileSync(storePath).toString()) : [];
+	const migrations = completedMigrations.length === 0 ? allMigrations : allMigrations.filter((file) => done.indexOf(file) === -1);
 	if (migrations.length === 0) {
 		console.log('Everything is up to date!');
 		return;
 	}
 	for (let i = 0; i < migrations.length; i++) {
-		await applyMigration(migrations[i]);
+		try {
+			await applyMigration(migrations[i]);
+			completedMigrations.push(migrations[i]);
+		} catch (error) {
+			writeFileSync(storePath, JSON.stringify(completedMigrations));
+			console.log(error);
+			return;
+		}
 	}
-}
-
-async function createMigrationsTable() {
-	db.execute(`
-        CREATE TABLE migrations (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            timestamp DATETIME NOT NULL
-        ) 
-    `);
+	writeFileSync(storePath, JSON.stringify(completedMigrations));
 }
 
 async function applyMigration(file) {
 	process.stdout.write(`[...] ${file}\r`);
 	const sql = readFileSync(`./migrations/${file}`).toString();
 	await db.executeMultiple(sql);
-	await db.execute({
-		sql: 'INSERT INTO `migrations` (name, timestamp) VALUES (?, ?)',
-		args: [file, Math.floor(Date.now() / 1000)],
-	});
 	process.stdout.write(`[DONE] ${file}\n`);
 }
 
