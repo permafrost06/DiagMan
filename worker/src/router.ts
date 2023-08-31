02c0b53d044e894a7e3e80ad0d9a05348b63e183
Saad <sakib.saad.khan@gmail.com>
2023-08-31 11:23:25 +0600

    fixed RequestHandler type

--- worker/src/router.ts
+++ worker/src/router.ts
@@ -1,22 +1,22 @@
-import { RouterType, withParams } from 'itty-router';
+import { IRequest, RouteHandler, RouterType, withParams } from 'itty-router';
 import { Env } from './worker';
 import JSONResponse from './utils/Response';
 import { addTest, deleteTest, listTests } from './routes/med-test';
 import { addRecord, listRecords } from './routes/records';
 
-export interface RequestEvent extends Record<string, any> {
+export interface RequestEvent {
 	request: Request;
 	env: Env;
 	res: JSONResponse;
 	method: Request['method'];
 	url: Request['url'];
 }
-export type RequestHandler = (event: RequestEvent) => Record<string, any> | undefined;
+export type RequestHandler = RouteHandler<IRequest & RequestEvent>;
 
 export const buildRouter = (router: RouterType) => {
 	router.post('/tests', addTest);
 	router.get('/tests', listTests);
-	router.delete('/tests/:id', withParams, deleteTest as any);
+	router.delete('/tests/:id', withParams, deleteTest);
 
 	router.post('/records', addRecord);
 	router.get('/records', listRecords);
