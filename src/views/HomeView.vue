351b0292d2e1cc1ef7824e60429c1b27599af359
Saad <sakib.saad.khan@gmail.com>
2023-09-19 22:07:54 +0600

    display auth if not authenticated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,58 +1,22 @@
 <script setup lang="ts">
-import { API_BASE, AUTH_TOKEN_KEY } from "@/helpers/config";
-import { fetchApi } from "@/helpers/http";
+import { useUser } from "@/stores/user";
 import { onMounted, ref } from "vue";
+import Login from "./auth/Login.vue";
+import Pin from "./auth/Pin.vue";
+import FullPageLoader from "@/components/base/FullPageLoader.vue";
 
-const user = ref();
-const loggingOut = ref(false);
+const loading = ref(true);
 
-const logout = async () => {
-    if (loggingOut.value) {
-        return;
-    }
-    loggingOut.value = true;
-    const res = await fetchApi(API_BASE + "/auth/logout", {
-        method: "POST",
-    });
-    loggingOut.value = false;
-    if (res.success) {
-        user.value = null;
-        localStorage.removeItem(AUTH_TOKEN_KEY);
-    }
-};
+const user = useUser();
 
 onMounted(async () => {
-    const res = await fetchApi(API_BASE + "/auth");
-    if (res.success) {
-        user.value = res.rows?.[0];
-    }
+    await user.sync();
+    loading.value = false;
 });
 </script>
 <template>
-    This is home <br />
-    <RouterLink :to="{ name: 'components' }">Components</RouterLink>
-    <RouterLink :to="{ name: 'tests' }">Tests</RouterLink>
-    <RouterLink :to="{ name: 'patients' }">Patients</RouterLink>
-    <br />
-    <hr />
-    <div v-if="user">
-        <p>Logged in as: {{ user.name }}</p>
-        <br />
-        <button @click="logout">
-            {{ loggingOut ? "Loading..." : "Logout" }}
-        </button>
-        <br />
-        <RouterLink :to="{ name: 'users' }">Users</RouterLink>
-    </div>
-    <div v-else>
-        <RouterLink :to="{ name: 'login' }">Login</RouterLink>
-        <RouterLink :to="{ name: 'register' }">Register</RouterLink>
-    </div>
+    <FullPageLoader v-if="loading" />
+    <Login v-else-if="!user.isLogged" />
+    <Pin v-else-if="!user.verified" />
+    <div v-else>Logged in</div>
 </template>
-<style>
-a,
-p {
-    display: block;
-    padding: 0 10px;
-}
-</style>
