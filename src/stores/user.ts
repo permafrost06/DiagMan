66526bb85316435b8c4ae4ff8f87f3e8bd6d2cf1
Saad <sakib.saad.khan@gmail.com>
2023-09-19 22:05:30 +0600

    added user store

---
+++ src/stores/user.ts
@@ -0,0 +1,48 @@
+import { API_BASE } from "@/helpers/config";
+import { fetchApi } from "@/helpers/http";
+import { defineStore } from "pinia";
+
+interface User {
+    id: number;
+    name: string;
+    email: string;
+    pin: number;
+}
+
+export const useUser = defineStore("user", {
+    state: (): User => ({
+        id: 0,
+        name: "",
+        email: "",
+        pin: 0,
+    }),
+
+    getters: {
+        isLogged(state) {
+            return state.id > 0;
+        },
+        verified(state) {
+            return state.pin != 0;
+        },
+    },
+
+    actions: {
+        async sync() {
+            try {
+                const res = await fetchApi(API_BASE + "/auth");
+                if (!res.success || !res.rows?.length) {
+                    throw new Error("Does not matter");
+                }
+                const user = res.rows[0];
+                this.id = user.id;
+                this.name = user.name;
+                this.email = user.email;
+                this.pin = 0;
+            } catch (error) {
+                this.id = 0;
+                this.name = "";
+                this.pin = 0;
+            }
+        },
+    },
+});
