12df8c77d02dcf3d941879764b64df27cb5b9148
Saad <sakib.saad.khan@gmail.com>
2023-10-03 09:55:11 +0600

    added SimpleInput

---
+++ src/components/form/SimpleInput.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { InputHTMLAttributes } from "vue";
+
+interface InputProps extends InputHTMLAttributes {
+    hint?: string;
+    hintType?: "error" | "warning" | "success" | "none";
+    label?: string;
+    class?: any;
+    unWrap?: boolean;
+}
+
+const props = withDefaults(defineProps<InputProps>(), {
+    type: "text",
+    hintType: "none",
+});
+</script>
+<template>
+    <div v-if="!unWrap" :class="[props.class, 'simple-input']">
+        <label class="si-label" v-if="label">{{ label }}</label>
+        <div class="si-field-col">
+            <input v-bind="$attrs" />
+
+            <label v-if="hint" class="hint">
+                {{ hint }}
+            </label>
+        </div>
+    </div>
+    <template v-else>
+        <label class="si-label" v-if="label">{{ label }}</label>
+        <div class="si-field-col">
+            <input v-bind="$attrs" />
+
+            <label v-if="hint" class="hint">
+                {{ hint }}
+            </label>
+        </div>
+    </template>
+</template>
