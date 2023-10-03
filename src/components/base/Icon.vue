15dbcb4b06cefeac70c0c79350b53f9740fb7be3
Saad <sakib.saad.khan@gmail.com>
2023-10-03 12:47:04 +0600

    added svg Icon helper

---
+++ src/components/base/Icon.vue
@@ -0,0 +1,36 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import type { SVGAttributes } from "vue";
+
+interface Props {
+    viewBox?: string | number;
+    class?: any;
+    height?: SVGAttributes["height"];
+    width?: SVGAttributes["width"];
+    size?: SVGAttributes["height"];
+}
+const props = defineProps<Props>();
+
+const refViewBox = props.viewBox || props.size || 20;
+
+const viewBox: string =
+    typeof refViewBox === "number" || refViewBox.indexOf(" ") < 0
+        ? `0 0 ${refViewBox} ${refViewBox}`
+        : refViewBox;
+
+const height = props.height || props.size;
+const width = props.width || props.size;
+</script>
+
+<template>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        :view-box="viewBox"
+        :height="height"
+        :width="width"
+        :class="props.class"
+    >
+        <slot />
+    </svg>
+</template>
