c56693d2a1c8a5b12164add3ac44726999417c01
Saad <sakib.saad.khan@gmail.com>
2023-10-14 10:59:01 +0600

    added settings close button

--- src/layout/SettingsLayout.vue
+++ src/layout/SettingsLayout.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Icon from "@/components/base/Icon.vue";
+</script>
 <template>
     <div class="settings-layout">
         <nav class="sidebar font-h">
@@ -79,115 +81,79 @@
         <div class="setting-page-area">
             <RouterView></RouterView>
         </div>
+        <RouterLink :to="{ name: 'home' }" class="home-url">
+            <Icon size="40" view-box="36">
+                <path
+                    fill="currentColor"
+                    d="m19.41 18l8.29-8.29a1 1 0 0 0-1.41-1.41L18 16.59l-8.29-8.3a1 1 0 0 0-1.42 1.42l8.3 8.29l-8.3 8.29A1 1 0 1 0 9.7 27.7l8.3-8.29l8.29 8.29a1 1 0 0 0 1.41-1.41Z"
+                    class="clr-i-outline clr-i-outline-path-1"
+                /><path fill="none" d="M0 0h36v36H0z" />
+            </Icon>
+        </RouterLink>
     </div>
 </template>
 
 <style lang="scss">
-.settings-page {
-    padding: 50px;
-}
-
-.settings-page .back-link {
-    color: var(--clr-black);
-    text-decoration: none;
-    padding: 3px 8px;
-    border: 1px solid var(--clr-black);
-    cursor: pointer;
-    gap: 10px;
-    font-size: var(-fs-base);
-    width: max-content;
-    font-weight: 500;
-}
-
-.settings-page .page-title {
-    margin-top: 15px;
-    font-weight: 500;
-}
-
-.settings-page .setting-links {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    padding: 50px;
-    gap: 100px;
-    max-width: 1300px;
-    margin: auto;
-}
-
-.settings-page .setting-link {
-    width: 220px;
-    height: 200px;
-    display: flex;
-    flex-flow: column;
-    justify-content: center;
-    align-items: center;
-    text-decoration: none;
-    font-size: var(--fs-md);
-    color: var(--clr-black);
-    font-weight: 500;
-    border: 1px solid;
-}
-.settings-page .setting-link img {
-    height: 48px;
-    width: 48px;
-    object-fit: contain;
-    margin-bottom: 20px;
-}
-
 .settings-layout {
     display: flex;
     height: 100%;
     width: 100%;
-}
 
-.settings-layout .sidebar {
-    margin: 0;
-    padding: 0;
-    background: var(--clr-white);
-    width: 330px;
-    font-weight: 500;
-    padding: 30px 20px;
-}
+    .sidebar {
+        margin: 0;
+        padding: 0;
+        background: var(--clr-white);
+        width: 330px;
+        font-weight: 500;
+        padding: 30px 20px;
+    }
 
-.settings-layout .sidebar .sidebar-top {
-    text-decoration: none;
-    color: var(--clr-black);
-    font-size: var(--fs-xl);
-    padding: 0 10px;
-    gap: 30px;
-}
+    .sidebar .sidebar-top {
+        text-decoration: none;
+        color: var(--clr-black);
+        font-size: var(--fs-xl);
+        padding: 0 10px;
+        gap: 30px;
+    }
 
-.settings-layout .sidebar ul {
-    margin: 0;
-    padding: 0;
-    border-bottom: 1px solid var(--clr-black);
-}
-.settings-layout .sidebar li {
-    list-style-type: none;
-    border-bottom: 1px solid var(--clr-white);
-}
+    .sidebar ul {
+        margin: 0;
+        padding: 0;
+        border-bottom: 1px solid var(--clr-black);
+    }
+    .sidebar li {
+        list-style-type: none;
+        border-bottom: 1px solid var(--clr-white);
+    }
 
-.settings-layout .sidebar-link {
-    display: block;
-    text-decoration: none;
-    color: var(--clr-black);
-    padding: 5px 10px;
-    font-size: var(--fs-md);
-}
-.settings-layout .sidebar .group-title {
-    opacity: 0.9;
-    padding: 0 10px;
-    padding-top: 10px;
-    font-weight: lighter;
-}
-.settings-layout .sidebar-link.router-link-exact-active {
-    background: var(--clr-black);
-    color: var(--clr-white);
-}
+    .sidebar-link {
+        display: block;
+        text-decoration: none;
+        color: var(--clr-black);
+        padding: 5px 10px;
+        font-size: var(--fs-md);
+    }
+    .sidebar .group-title {
+        opacity: 0.9;
+        padding: 0 10px;
+        padding-top: 10px;
+        font-weight: lighter;
+    }
+    .sidebar-link.router-link-exact-active {
+        background: var(--clr-black);
+        color: var(--clr-white);
+    }
 
-.settings-layout .setting-page-area {
-    padding: 30px;
-    flex-grow: 1;
-    overflow-y: scroll;
+    .setting-page-area {
+        padding: 30px;
+        padding-right: 50px;
+        flex-grow: 1;
+        overflow-y: auto;
+    }
+    .home-url {
+        position: absolute;
+        right: 10px;
+        top: 23px;
+    }
 }
 </style>
