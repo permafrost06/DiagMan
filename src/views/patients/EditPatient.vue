bd6cb7ff63155f65b0fc4e4f6edecf9de30607cc
Saad <sakib.saad.khan@gmail.com>
2023-10-17 14:52:23 +0600

    fix: remove title while loading

--- src/views/patients/EditPatient.vue
+++ src/views/patients/EditPatient.vue
@@ -33,16 +33,18 @@ loadPatient();
 <template>
     <PatientForm :to-edit="patient" v-if="patient" :headless="headless" />
     <div v-else class="add-patient-page">
-        <h1 class="fs-2xl">Update Patient</h1>
-        <RouterLink :to="{ name: 'home' }" class="home-url">
-            <Icon size="40" view-box="36">
-                <path
-                    fill="currentColor"
-                    d="m19.41 18l8.29-8.29a1 1 0 0 0-1.41-1.41L18 16.59l-8.29-8.3a1 1 0 0 0-1.42 1.42l8.3 8.29l-8.3 8.29A1 1 0 1 0 9.7 27.7l8.3-8.29l8.29 8.29a1 1 0 0 0 1.41-1.41Z"
-                    class="clr-i-outline clr-i-outline-path-1"
-                /><path fill="none" d="M0 0h36v36H0z" />
-            </Icon>
-        </RouterLink>
+        <div v-if="!headless">
+            <h1 class="fs-2xl">Update Patient</h1>
+            <RouterLink :to="{ name: 'home' }" class="home-url">
+                <Icon size="40" view-box="36">
+                    <path
+                        fill="currentColor"
+                        d="m19.41 18l8.29-8.29a1 1 0 0 0-1.41-1.41L18 16.59l-8.29-8.3a1 1 0 0 0-1.42 1.42l8.3 8.29l-8.3 8.29A1 1 0 1 0 9.7 27.7l8.3-8.29l8.29 8.29a1 1 0 0 0 1.41-1.41Z"
+                        class="clr-i-outline clr-i-outline-path-1"
+                    /><path fill="none" d="M0 0h36v36H0z" />
+                </Icon>
+            </RouterLink>
+        </div>
         <div class="ap-body">
             <Loading v-if="isLoading" size="60" />
             <p v-else class="form-alert error">
