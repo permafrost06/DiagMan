e05894745c0b34cf0e296551ba8ebcabb2ce85db
Saad <sakib.saad.khan@gmail.com>
2023-11-10 17:54:15 +0600

    fix: check for invoice before closing form

--- src/views/patients/AddPatient.vue
+++ src/views/patients/AddPatient.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import router from "@/router";
 import PatientForm from "./PatientForm.vue";
-const onSuccess = () => {
+const onSuccess = (_x: any, _y: any, invoice: boolean) => {
+    if (invoice) {
+        return;
+    }
     router.push({
         name: "home",
     });
