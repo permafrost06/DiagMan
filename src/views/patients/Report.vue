55f7ad6d2908b470ae3276856ed1034528cbee53
Sakibur Rahman Khan <sakib.saad.khangmail.com>
2024-01-15 17:04:25 +0600

    fix: note not loading

--- src/views/patients/Report.vue
+++ src/views/patients/Report.vue
@@ -87,7 +87,10 @@ watch(noteFieldVisible, async () => {
     await nextTick();
     if (noteFieldVisible.value) {
         if (patient.value?.note) {
-            quillInstances.note.setContents(patient.value.note.ops);
+            console.log(patient.value.note);
+            quillInstances.note.setContents(
+                JSON.parse(patient.value.note || "{}").ops
+            );
         }
     }
 });
