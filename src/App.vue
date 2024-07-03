e7656325a5ca37fecdffff06d6e0d89672e35b6c
Saad <sakib.saad.khan@gmail.com>
2024-07-03 20:29:03 +0600

    remove notify test

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,5 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-import notify from "./components/notification";
-import { onMounted } from "vue";
-onMounted(() => {
-    let c = 0;
-    document.addEventListener("click", (evt) => {
-        c++;
-        [
-            "left", //"right"
-        ].forEach((x) => {
-            [
-                "top", //"bottom"
-            ].forEach((y) => {
-                const side = `${y}-${x}`;
-                const duration = evt.shiftKey ? 0 : 2000;
-
-                notify({
-                    message: `Test ${side}: ${c}`,
-                    position: side as any,
-                    duration,
-                });
-            });
-        });
-    });
-});
 </script>
 
 <template>
