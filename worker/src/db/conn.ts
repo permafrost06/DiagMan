a29f52b1282250e9463460d1622793a7c7887572
Saad <sakib.saad.khan@gmail.com>
2024-07-08 05:30:24 +0600

    authToken can be undefined in local server

--- worker/src/db/conn.ts
+++ worker/src/db/conn.ts
@@ -8,9 +8,6 @@ export const getLibsqlClient = (env: Env): LibsqlClient => {
 	}
 
 	const authToken = env.TURSO_AUTH_TOKEN?.trim();
-	if (authToken === undefined) {
-		throw new Error('TURSO_AUTH_TOKEN env var is not defined');
-	}
 
 	return createClient({ url, authToken });
 };
