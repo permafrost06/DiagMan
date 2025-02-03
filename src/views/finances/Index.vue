c8d25d0a931c87626ff349830f05375480cb9a56
Saad <sakib.saad.khan@gmail.com>
2025-02-03 19:50:29 +0600

    display dynamic finance

--- src/views/finances/Index.vue
+++ src/views/finances/Index.vue
@@ -7,56 +7,187 @@ import MonthSelector, {
 import TakaIcon from "@/Icons/taka.svg";
 
 import router from "@/router";
-import { ref } from "vue";
+import { computed, onMounted, ref, watch } from "vue";
 import Chart from "@/components/chart/Chart.vue";
 import type { BarChartData } from "@/components/chart/BarChart";
 import { DonutChartData } from "@/components/chart/DonutChart";
+import { fetchApi } from "@/helpers/http";
+import { API_BASE } from "@/helpers/config";
+
+const loading = ref(false);
+const reqCtrl = ref<AbortController | null>(null);
 const selectedMonths = ref<MonthSelection>({
     year: new Date().getFullYear(),
     start: new Date().getMonth(),
 });
 
-const dataGroups = ref<BarChartData>({
-    labels: ["Week1", "Week2", "Week3", "Week4"],
-    keys: [
-        { key: "histo", color: "#6C63FF", label: "Histopathology" },
-        { key: "cyto", color: "#D3D3D3", label: "Cytopathology" },
-    ],
-    values: [
-        { histo: 100, cyto: 200 },
-        { histo: 150, cyto: 250 },
-        { histo: 180, cyto: 300 },
-        { histo: 250, cyto: 200 },
-    ],
+interface SaleData {
+    total: number;
+    last: number;
+    avg: number;
+}
+
+interface TestItem {
+    name: string;
+    amount: number;
+}
+interface BarItem {
+    type: "histo" | "cyto";
+    week: string;
+    total_sum: number;
+}
+const apiData = ref<{
+    revenue: SaleData;
+    discount: SaleData;
+    tests: TestItem[];
+    barChart: BarItem[];
+} | null>(null);
+
+const fetchData = async () => {
+    if (loading.value) {
+        reqCtrl.value?.abort();
+        reqCtrl.value = new AbortController();
+    }
+    if (!reqCtrl.value) {
+        reqCtrl.value = new AbortController();
+    }
+    loading.value = true;
+    try {
+        const res = await fetchApi(
+            `${API_BASE}/finances?from=${selectedMonths.value.year}-${selectedMonths.value.start}`,
+            {
+                signal: reqCtrl.value?.signal,
+            }
+        );
+        if (!res.success) {
+            return;
+        }
+        apiData.value = res.data;
+    } catch (_) {
+        // keep
+    } finally {
+        loading.value = false;
+    }
+};
+
+onMounted(() => {
+    fetchData();
 });
 
-const data: DonutChartData = [
-    {
-        label: "Test One",
-        value: 30,
-        color: "#A463F2",
-    },
-    {
-        label: "Test two",
-        value: 23,
-        color: "#C4FF00",
-    },
-    {
-        label: "Test three",
-        value: 18,
-        color: "#00BFFF",
-    },
-    {
-        label: "Test Four",
-        value: 17,
-        color: "#666666",
-    },
-    {
-        label: "Test Five",
-        value: 13,
-        color: "#333",
-    },
-];
+watch(selectedMonths, () => {
+    fetchData();
+});
+
+const computeSale = (data?: SaleData) => {
+    if (!data) {
+        return {
+            total: 0,
+            last: 0,
+            avg: 0,
+        };
+    }
+
+    const { total, last, avg } = data;
+    let avg_growth = 0;
+    if (!avg) {
+        avg_growth = total ? 100 : 0;
+    } else {
+        avg_growth = ((total - avg) / avg) * 100;
+    }
+    let last_growth = 0;
+    if (!last) {
+        last_growth = total ? 100 : 0;
+    } else {
+        last_growth = ((total - last) / last) * 100;
+    }
+
+    const avg_up = avg_growth >= 0;
+    const last_up = last_growth >= 0;
+    return {
+        amount: (total / 100).toFixed(2),
+        last: (last_up ? "+" : "") + last_growth.toFixed(2),
+        avg: (avg_up ? "+" : "") + avg_growth.toFixed(2),
+        avg_up,
+        last_up,
+    };
+};
+
+const revenue = computed(() => computeSale(apiData.value?.revenue));
+const discount = computed(() => computeSale(apiData.value?.discount));
+
+const barChartData = computed<BarChartData>(() => {
+    if (!apiData.value || apiData.value.barChart.length == 0) {
+        return {
+            labels: ["01", "02", "03", "04", "05", "06", "07"],
+            keys: [
+                { key: "histo", color: "#6C63FF", label: "Histopathology" },
+                { key: "cyto", color: "#D3D3D3", label: "Cytopathology" },
+            ],
+            values: Array(7)
+                .fill(0)
+                .map(() => ({ histo: 0, cyto: 0 })),
+        };
+    }
+    const labels: string[] = [];
+    const values: BarChartData["values"] = [];
+
+    const weeks: string[] = [];
+    const vals: Record<string, BarChartData["values"][number]> = {};
+
+    apiData.value.barChart.forEach((item) => {
+        labels.push(item.week.split("-")[1]);
+        weeks.push(item.week);
+        let val = vals[item.week];
+        if (!val) {
+            val = { histo: 0, cyto: 0 };
+            vals[item.week] = val;
+        }
+        val[item.type] = Math.round(item.total_sum / 100);
+    });
+
+    weeks.forEach((week) => {
+        values.push({
+            histo: vals[week].histo || 0,
+            cyto: vals[week].cyto || 0,
+        });
+    });
+
+    return {
+        labels,
+        values,
+        keys: [
+            { key: "histo", color: "#6C63FF", label: "Histopathology" },
+            { key: "cyto", color: "#D3D3D3", label: "Cytopathology" },
+        ],
+    };
+});
+
+const donutChartData = computed<DonutChartData>(() => {
+    if (!apiData.value || apiData.value.tests.length == 0) {
+        return [
+            {
+                label: "None",
+                value: 100,
+                color: "#A463F2",
+            },
+        ];
+    }
+
+    const values: DonutChartData = [];
+
+    const total = apiData.value.tests.reduce((acc, item) => {
+        return acc + item.amount;
+    }, 0);
+
+    apiData.value.tests.forEach((item, i) => {
+        values.push({
+            label: item.name,
+            value: Math.round((item.amount / total) * 100),
+            color: `hsl(${i * 30}, 50%, 50%)`,
+        });
+    });
+    return values;
+});
 </script>
 <template>
     <div class="finances-page">
@@ -79,30 +210,66 @@ const data: DonutChartData = [
         </div>
         <div class="finance-grid">
             <div>
-                <MonthSelector :as-block="true" v-model="selectedMonths" />
+                <MonthSelector
+                    :range-select="false"
+                    :as-block="true"
+                    v-model="selectedMonths"
+                />
                 <h2 class="finance-months">{{ selectedMonths.formatted }}</h2>
                 <div class="finance-info">
                     <h3>Revenue</h3>
                     <p class="finance-info-value">
                         <TakaIcon />
-                        100010/-
+                        {{ revenue.amount }}/-
                     </p>
-                    <p class="finance-info-up">&#11014; +10% from last month</p>
-                    <p class="finance-info-down">&#11015; -2% from average</p>
+                    <p
+                        :class="{
+                            'finance-info-loading': loading,
+                            'finance-info-up': revenue.last_up,
+                            'finance-info-down': !revenue.last_up,
+                        }"
+                    >
+                        {{ loading ? "" : revenue.last + "% from last month" }}
+                    </p>
+                    <p
+                        :class="{
+                            'finance-info-loading': loading,
+                            'finance-info-up': revenue.avg_up,
+                            'finance-info-down': !revenue.avg_up,
+                        }"
+                    >
+                        {{ loading ? "" : revenue.avg + "% from average" }}
+                    </p>
                 </div>
                 <div class="finance-info">
                     <h3>Discount Given</h3>
                     <p class="finance-info-value">
                         <TakaIcon />
-                        100010/-
+                        {{ discount.amount }}/-
                     </p>
-                    <p class="finance-info-up">&#11014; +10% from last month</p>
-                    <p class="finance-info-down">&#11015; -2% from average</p>
+                    <p
+                        :class="{
+                            'finance-info-loading': loading,
+                            'finance-info-up': discount.last_up,
+                            'finance-info-down': !discount.last_up,
+                        }"
+                    >
+                        {{ loading ? "" : discount.last + "% from last month" }}
+                    </p>
+                    <p
+                        :class="{
+                            'finance-info-loading': loading,
+                            'finance-info-up': discount.avg_up,
+                            'finance-info-down': !discount.avg_up,
+                        }"
+                    >
+                        {{ loading ? "" : discount.avg + "% from average" }}
+                    </p>
                 </div>
             </div>
             <div class="charts-area">
-                <Chart class="chart-item" type="bar" :data="dataGroups" />
-                <Chart class="chart-item" type="donut" :data="data" />
+                <Chart class="chart-item" type="bar" :data="barChartData" />
+                <Chart class="chart-item" type="donut" :data="donutChartData" />
             </div>
         </div>
     </div>
@@ -159,9 +326,42 @@ const data: DonutChartData = [
         }
         &-up {
             color: var(--clr-success);
+            &::before {
+                content: "\2B06";
+            }
         }
         &-down {
             color: var(--clr-danger);
+            &::before {
+                content: "\2B07";
+            }
+        }
+
+        &-loading {
+            color: var(--clr-black);
+            position: relative;
+            height: 10px;
+            &::before {
+                content: "";
+                position: absolute;
+                top: 0;
+                left: 0;
+                height: 100%;
+                width: 15px;
+                background: var(--clr-black);
+                opacity: 0.4;
+            }
+
+            &::after {
+                content: "";
+                position: absolute;
+                top: 0;
+                left: 20px;
+                height: 100%;
+                width: 100px;
+                background: var(--clr-black);
+                opacity: 0.4;
+            }
         }
     }
 
